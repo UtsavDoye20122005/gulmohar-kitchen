a9893cca5ea4b9b62391cd154856dc3809e05e71
Utsav Doye <utsavdoye07@gmail.com>
2026-09-24 22:09:45 +0530

    Fix tabs orientation breaking gallery

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -14,7 +14,7 @@ function Tabs({
       data-slot="tabs"
       data-orientation={orientation}
       className={cn(
-        "group/tabs flex gap-3 data-horizontal:flex-col",
+        "group/tabs flex gap-3 data-[orientation=horizontal]:flex-col",
         className
       )}
       {...props}
